d8033817eb093d1af7f457962af16ad53882de37
Pritam Saha <saha7pritam@gmail.com>
2024-07-31 12:06:36 +0530

    Add files via upload

---
+++ src/components/Accordion.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import data from '../Constants/data'
+
+const Accordion = () => {
+
+  const [selected, setSelected] = useState(null)
+  const [enableMultiselection, setEnableMultiselection] = useState(false)
+  const [multipleIds, setMultipleIds] = useState([])
+
+
+    const handleSingleclick = (getCurrentId) =>{
+        console.log(getCurrentId)
+        setSelected(getCurrentId === selected ? null : getCurrentId)
+    }
+
+    function handleMultipleClick(getCurrentId){
+      let copyOfMultipleIds = [...multipleIds]
+
+      let findIndexOfCurrentId = copyOfMultipleIds.indexOf(getCurrentId)
+
+      if (findIndexOfCurrentId === -1) copyOfMultipleIds.push(getCurrentId)
+        else copyOfMultipleIds.splice(findIndexOfCurrentId, 1)
+
+      setMultipleIds(copyOfMultipleIds)
+
+    }
+
+
+  return (
+    <div className='wrapper'>
+    <button onClick={()=> setEnableMultiselection(!enableMultiselection) } >Multiple Selection</button>
+        <div className='accordian'>
+        <h1 className='header'>Accordian List</h1>
+          { (data && data.length > 0) ? data.map((item)=> <div className='item' key={item.id}>
+              <div 
+              className='title' 
+              onClick={ enableMultiselection ? ()=>handleMultipleClick(item.id) : ()=>handleSingleclick(item.id)}
+              >
+                <h3>{item.question}</h3>
+                <span>+</span>
+              </div>
+              { (selected === item.id) || (multipleIds.indexOf(item.id) !== -1) ? <div className='content'>{item.answer}</div> : null}
+          </div>
+          ) : <div>Sorry, No Data Available </div>}
+        </div>
+    </div>
+  )
+}
+
+export default Accordion
